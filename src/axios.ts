6ca117778212f4d63ef8c19a664c43f1dba0e544
Marco Benitez <benitez4721@gmail.com>
2025-01-27 08:06:54 -0400

    chore: add polygon api key env variable

--- src/axios.ts
+++ src/axios.ts
@@ -4,9 +4,10 @@ const apiClient = axios.create({
   baseURL: "https://api.polygon.io/",
 });
 
+
 apiClient.interceptors.request.use(
   (config) => {
-    config.headers.Authorization = `Bearer 0cOq2zQNG1gW4Fsy4kYLG7_YMBzrz5bu`;
+    config.headers.Authorization = `Bearer ${import.meta.env.VITE_POLYGON_API_KEY}`;
 
     return config;
   },
